f36576657dfb7fffdec57d2d4ba763d15f8aed15
Murat Ertik <dmuratertik1@gmail.com>
2025-01-17 06:17:31 -0800

    first commit

---
+++ src/main/java/com/p1/projectmanagemanagementsystem/ProjectmanagemanagementsystemApplication.java
@@ -0,0 +1,13 @@
+package com.p1.projectmanagemanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectmanagemanagementsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectmanagemanagementsystemApplication.class, args);
+	}
+
+}
