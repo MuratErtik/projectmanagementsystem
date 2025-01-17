fed5047003ff018b66b696a8ef9ee30c4031c7c0
Murat Ertik <dmuratertik1@gmail.com>
2025-01-17 09:06:29 -0800

    JwtTokenValidator inherit from OncePerRequestFilter

--- src/main/java/com/p1/AppConfig/JwtTokenValidator.java
+++ src/main/java/com/p1/AppConfig/JwtTokenValidator.java
@@ -1,5 +1,21 @@
 package com.p1.AppConfig;
 
-public class JwtTokenValidator {
+import java.io.IOException;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class JwtTokenValidator extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+                
+        
+    }
 
 }
