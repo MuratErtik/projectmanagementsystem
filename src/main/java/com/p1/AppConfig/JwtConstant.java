29e6d0c8115c302a4fb8664c64a20eafcac300a0
Murat Ertik <dmuratertik1@gmail.com>
2025-01-17 10:02:22 -0800

    2 constant key have been added!

--- src/main/java/com/p1/AppConfig/JwtConstant.java
+++ src/main/java/com/p1/AppConfig/JwtConstant.java
@@ -1,6 +1,11 @@
 package com.p1.AppConfig;
 
 public class JwtConstant {
-    
+    public static final String SECRETE_KEY="asjhbdjhbdsjhsdjhsbhjdsbhjds";
+
+    public static final String JWT_HEADER="Authorizaton";
+
+
+
 
 }
